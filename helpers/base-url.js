1617dd1bcec4235b11f2aa20bb217b7a7e81780d
Roynaldi <52753423+roynaldi3301@users.noreply.github.com>
2022-03-04 08:22:48 +0700

    ganti url

--- helpers/base-url.js
+++ helpers/base-url.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: 'https://otakudesu.moe/',
+    baseUrl: 'https://otakudesu.live/',
     completeAnime:'complete-anime/',
     onGoingAnime:'ongoing-anime/',
     schedule:'jadwal-rilis/',
